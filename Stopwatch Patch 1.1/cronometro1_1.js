8fb0cdaf31761089217dd5312be42acec0baad8e
Gabriel Machado <101916850+machado001@users.noreply.github.com>
2022-04-25 15:50:17 -0300

    Update cronometro1_1.js

--- Stopwatch Patch 1.1/cronometro1_1.js
+++ Stopwatch Patch 1.1/cronometro1_1.js
@@ -2,21 +2,21 @@ let milsec = 00;
 let min = 00;
 let sec = 00;
 let time;
-let start;
+let iniciar;
 
 
 
 
 function start() {
     document.getElementById('start').disabled = 'disabled'
-    start = setInterval(tempo, 10)
-    return start;
+    iniciar = setInterval(tempo, 10)
+    return iniciar;
 
 }
 
 function stop() {
     document.getElementById('start').disabled = false
-    clearInterval(start)
+    clearInterval(iniciar)
     
 
 }
@@ -28,11 +28,11 @@ function reset() {
     milsec = 00
     sec = 00
     min = 00
-    clearInterval(start)
+    clearInterval(iniciar)
 }
 
 function tempo() {
-    let time = document.getElementById('time').innerHTML = `${min.toFixed()}:${sec}:${milsec++}`
+    let time = document.getElementById('time').innerHTML = `${min}:${sec}:${milsec++}`
 
     if (milsec === 100) {
         milsec = 0
